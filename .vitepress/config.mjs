ed4165282669135b04689e2f1fbf0f4be42e3a1c
waylonzheng ovo <waylonzheng@shein.com>
2024-03-06 10:21:11 +0800

    deploy: 分别部署github page 和vercel

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -1,48 +1,50 @@
-import { defineConfig } from 'vitepress'
-
+import { defineConfig, loadEnv  } from 'vitepress'
 // https://vitepress.dev/reference/site-config
-export default defineConfig({
-  title: "OVO Tab",
-  description: "自定义的新标签页，支持管理书签、海量壁纸、数据云同步等多种功能",
-  // 主题相关配置
-  head: [['link', { rel: 'icon', href: '/icon.png' }]],
-  assetsDir: './public',
-  outDir:'./docs',
-  // base: '/OVO-Tab-Official-Web/',
-  lastUpdated: true,
-  themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
-    logo: { light: '/logo-black.svg', dark: '/logo.svg', },
-    nav: [
-      { text: '首页', link: '/' },
-      { text: '网页版', link: 'https://www.waylon.online/ovotab/newtab.html' },
-    ],
-
-    sidebar: [
-      {
-        text: '安装',
-        items: [
-          { text: '插件介绍', link: '/installationHelp' },
-          { text: 'chrome安装', link: '/installationHelp' }
-        ]
-      },
-      {
-        text: '使用',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      },
-      {
-        text: '关于',
-        items: [
-          { text: '隐私政策', link: '/privacyPolicy' },
-        ]
-      }
-    ],
-
-    // socialLinks: [
-    //   { icon: 'github', link: 'https://www.waylon/ovotab/assets/icon-s0JiMFF1.png' }
-    // ]
+export default defineConfig((({ command, mode }) => {
+  const { VITE_BASE_DIR } = loadEnv(mode, "./")
+  return {
+    title: "OVO Tab",
+    description: "自定义的新标签页，支持管理书签、海量壁纸、数据云同步等多种功能",
+    // 主题相关配置
+    head: [['link', { rel: 'icon', href: '/icon.png' }]],
+    assetsDir: './public',
+    outDir:'./docs',
+    base: VITE_BASE_DIR,
+    lastUpdated: true,
+    themeConfig: {
+      // https://vitepress.dev/reference/default-theme-config
+      logo: { light: '/logo-black.svg', dark: '/logo.svg', },
+      nav: [
+        { text: '首页', link: '/' },
+        { text: '网页版', link: 'https://www.waylon.online/ovotab/newtab.html' },
+      ],
+  
+      sidebar: [
+        {
+          text: '安装',
+          items: [
+            { text: '插件介绍', link: '/installationHelp' },
+            { text: 'chrome安装', link: '/installationHelp' }
+          ]
+        },
+        {
+          text: '使用',
+          items: [
+            { text: 'Markdown Examples', link: '/markdown-examples' },
+            { text: 'Runtime API Examples', link: '/api-examples' }
+          ]
+        },
+        {
+          text: '关于',
+          items: [
+            { text: '隐私政策', link: '/privacyPolicy' },
+          ]
+        }
+      ],
+  
+      // socialLinks: [
+      //   { icon: 'github', link: 'https://www.waylon/ovotab/assets/icon-s0JiMFF1.png' }
+      // ]
+    }
   }
-})
+}))
